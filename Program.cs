292099d5fec416558a9cbac7d224267d39b75695
Maksim Drobyak <maksim_d_2016@mail.ru>
2026-04-22 22:25:06 +0300

    Initial NelderMeadCore

---
+++ Program.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NelderMeadParser;
+
+namespace NelderMeadCore
+{
+    public class Simplex
+    {
+        public Vector[] Points;
+
+        public Simplex(Vector[] points)
+        {
+            Points = new Vector[points.Length];
+            for (int i = 0; i < points.Length; i++)
+            {
+                Points[i] = points[i];
+            }
+        }
+
+        public void SortPointsByResult(Function goalFunction)
+        {
+            double[] functionResults = new double[Points.Length];
+            for (int i = 0; i < Points.Length; i++)
+            {
+                try
+                {
+                    functionResults[i] = goalFunction.Evaluate(Points[i].Coordinates);
+                }
+                catch
+                {
+                    functionResults[i] = double.PositiveInfinity;
+                }
+            }
+            Array.Sort(functionResults, Points);
+        }
+
+        public void Shrink(double shrinkCoefficient)
+        {
+            for (int i = 1; i < Points.Length; i++)
+            {
+                Points[i] = Points[0] + (Points[i] - Points[0]) * shrinkCoefficient;
+            }
+        }
+    }
+
+    public class NelderMeadAlgorithm
+    {
+        public double ReflectionCoefficient;
+        public double ExpansionCoefficient;
+        public double ContractCoefficient;
+        public Simplex CurrentSimplex;
+        public Function GoalFunction;
+        public bool EndReached;
+
+        public NelderMeadAlgorithm(double alpha, double beta, double gamma, Simplex simplex, Function goalFunction)
+        {
+            ReflectionCoefficient = alpha;
+            ExpansionCoefficient = beta;
+            ContractCoefficient = gamma;
+            CurrentSimplex = simplex;
+            GoalFunction = goalFunction;
+            EndReached = false;
+        }
+
+        private double SafeEvaluate(Vector v)
+        {
+            try
+            {
+                return GoalFunction.Evaluate(v.Coordinates);
+            }
+            catch
+            {
+                return double.PositiveInfinity;
+            }
+        }
+
+        public void NextIteration()
+        {
+            Vector bestpoint = CurrentSimplex.Points[0];
+            Vector okpoint = CurrentSimplex.Points[CurrentSimplex.Points.Length - 2];
+            Vector badpoint = CurrentSimplex.Points[CurrentSimplex.Points.Length - 1];
+
+            Vector sum = Vector.Sum(CurrentSimplex.Points);
+            Vector midpoint = (sum - badpoint) / (CurrentSimplex.Points.Length - 1);
+
+            Vector reflected = midpoint + (midpoint - badpoint) * ReflectionCoefficient;
+
+            double f_reflected = SafeEvaluate(reflected);
+            double f_best = SafeEvaluate(bestpoint);
+            double f_ok = SafeEvaluate(okpoint);
+            double f_bad = SafeEvaluate(badpoint);
+
+            if (f_reflected < f_best)
+            {
+                Vector expanded = midpoint + (reflected - midpoint) * ExpansionCoefficient;
+                double f_expanded = SafeEvaluate(expanded);
+                if (f_expanded < f_best)
+                {
+                    badpoint = expanded;
+                }
+                else
+                {
+                    badpoint = reflected;
+                }
+            }
+            else
+            {
+                if (f_reflected < f_ok)
+                {
+                    badpoint = reflected;
+                }
+                else
+                {
+                    if (f_reflected < f_bad)
+                    {
+                        badpoint = reflected;
+                    }
+                    Vector contracted = midpoint + (badpoint - midpoint) * ContractCoefficient;
+                    double f_contracted = SafeEvaluate(contracted);
+                    if (f_contracted < f_bad)
+                    {
+                        badpoint = contracted;
+                    }
+                    else
+                    {
+                        CurrentSimplex.Points[CurrentSimplex.Points.Length - 1] = badpoint;
+                        CurrentSimplex.Shrink(0.5);
+                    }
+                }
+            }
+
+            CurrentSimplex.Points[CurrentSimplex.Points.Length - 1] = badpoint;
+            CurrentSimplex.SortPointsByResult(GoalFunction);
+
+            Vector avg = Vector.Sum(CurrentSimplex.Points) / CurrentSimplex.Points.Length;
+            double dispersion = (avg - CurrentSimplex.Points[0]).Norm();
+            if (dispersion < 0.000001)
+                EndReached = true;
+        }
+    }
+}
---
+++ Vector.cs
@@ -0,0 +1,107 @@
+﻿using System;
+
+namespace NelderMeadCore
+{
+    public class Vector
+    {
+        public double[] Coordinates;
+
+        public Vector(int size)
+        {
+            Coordinates = new double[size];
+            for (int i = 0; i < size; i++)
+            {
+                Coordinates[i] = 0;
+            }
+        }
+
+        public Vector(double[] coordinates)
+        {
+            Coordinates = new double[coordinates.Length];
+            for (int i = 0; i < coordinates.Length; i++)
+            {
+                Coordinates[i] = coordinates[i];
+            }
+        }
+
+        public static Vector Sum(Vector[] vectors)
+        {
+            Vector sum = new Vector(vectors[0].Coordinates.Length);
+            for (int i = 0; i < vectors.Length; i++)
+            {
+                sum += vectors[i];
+            }
+            return sum;
+        }
+
+        public double Norm()
+        {
+            double norm = 0;
+            for (int i = 0; i < Coordinates.Length; i++)
+            {
+                norm += Math.Pow(Coordinates[i], 2);
+            }
+            return Math.Sqrt(norm);
+        }
+
+
+        public static Vector operator +(Vector a, Vector b)
+        {
+            if (a.Coordinates.Length != b.Coordinates.Length)
+                throw new ArgumentException("Vector lengths must match");
+
+            Vector result = new Vector(a.Coordinates.Length);
+            for (int i = 0; i < a.Coordinates.Length; i++)
+            {
+                result.Coordinates[i] = a.Coordinates[i] + b.Coordinates[i];
+            }
+            return result;
+        }
+
+        public static Vector operator -(Vector a, Vector b)
+        {
+            if (a.Coordinates.Length != b.Coordinates.Length)
+                throw new ArgumentException("Vector lengths must match");
+
+            Vector result = new Vector(a.Coordinates.Length);
+            for (int i = 0; i < a.Coordinates.Length; i++)
+            {
+                result.Coordinates[i] = a.Coordinates[i] - b.Coordinates[i];
+            }
+            return result;
+        }
+
+        public static Vector operator *(Vector v, double scalar)
+        {
+            Vector result = new Vector(v.Coordinates);
+            for (int i = 0; i < v.Coordinates.Length; i++)
+            {
+                result.Coordinates[i] *= scalar;
+            }
+            return result;
+        }
+
+        public static Vector operator *(double scalar, Vector v)
+        {
+            return v * scalar;
+        }
+
+        public static Vector operator /(Vector v, double scalar)
+        {
+            if (scalar == 0)
+                throw new DivideByZeroException("Division by zero");
+
+            Vector result = new Vector(v.Coordinates);
+            for (int i = 0; i < v.Coordinates.Length; i++)
+            {
+                result.Coordinates[i] /= scalar;
+            }
+            return result;
+        }
+
+        public static Vector operator -(Vector v)
+        {
+            return v * -1;
+        }
+    }
+}
